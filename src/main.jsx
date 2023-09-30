e11787244c92bb8be53ed4f668d2054c91d8fd48
Suman Dhungana <dhunganasuman226@gmail.com>
2023-09-30 10:14:21 +0545

    update homepage

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App.jsx';
+import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
